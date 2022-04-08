21bd09dc086612b0b2638bf871ca0bd43ccf5587
John Paul Otim <johnpaul.otim@muehlbauer.de>
2022-04-08 16:44:58 -0600

    feat: first commit
    - first commit including most of tutorial implementation and some additions

---
+++ app/src/main/java/io/github/jhnplotim/test/testepoxy/package-info.java
@@ -0,0 +1,6 @@
+package io.github.jhnplotim.test.testepoxy;
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern;
+
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "item")
+interface config {}
